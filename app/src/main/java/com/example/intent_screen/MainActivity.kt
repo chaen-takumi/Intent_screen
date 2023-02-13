b936b3161aaf936a6961e4d874b945459ff1a0d2
chaen-takumi <chaen.ta1227@gmail.com>
2023-02-13 20:22:11 +0900

    code push

--- app/src/main/java/com/example/intent_screen/MainActivity.kt
+++ app/src/main/java/com/example/intent_screen/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.intent_screen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart: Button = findViewById(R.id.btnStart)
+
+        btnStart.setOnClickListener {
+
+            val intent =Intent(this,SecondActibity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/intent_screen/SecondActibity.kt
+++ app/src/main/java/com/example/intent_screen/SecondActibity.kt
@@ -1,11 +1,19 @@
 package com.example.intent_screen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SecondActibity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second_actibity)
+
+        val btnBack: Button = findViewById(R.id.btnBack)
+
+        btnBack.setOnClickListener {
+            finish()
+        }
     }
 }
